5e7f0ba158197cf6af0b6b430a71380feb592847
balmanp <114126470+balmanp@users.noreply.github.com>
2024-11-25 15:27:42 +0700

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -24,7 +24,7 @@ exports.config = {
             {
                 outputDir: 'allure-results',
                 disableWebdriverStepsReporting: true,
-                disableWebdriverScreenshotsReporting: true,
+                disableWebdriverScreenshotsReporting: false,
             }
         ]
     ],
